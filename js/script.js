06efe9d2ec9a7586eae6731b2f2a4bd1f6a79d5e
PCosminC <rekpilator@hotmail.com>
2023-09-17 11:50:29 +0200

    feat: Finalizado

--- js/script.js
+++ js/script.js
@@ -1,144 +1,128 @@
-let correctWord, timer, lives;
-const words = [
-    {
-        "word": "Casa",
-        "hint": "Lugar donde vives."
-    },
-    {
-        "word": "Perro",
-        "hint": "Un amigo peludo."
-    },
-    {
-        "word": "Flor",
-        "hint": "Puede ser rosa o margarita."
-    },
-    {
-        "word": "Sol",
-        "hint": "Fuente de luz y calor en el cielo."
-    },
-    {
-        "word": "Lago",
-        "hint": "Cuerpo de agua más pequeño que un océano."
-    },
-    {
-        "word": "Bicicleta",
-        "hint": "Medio de transporte con dos ruedas."
-    },
-    {
-        "word": "Computadora",
-        "hint": "Dispositivo para navegar en internet."
-    },
-    {
-        "word": "Jardín",
-        "hint": "Espacio con flores y plantas."
-    },
-    {
-        "word": "Guitarra",
-        "hint": "Instrumento musical de cuerdas."
-    },
-    {
-        "word": "Reloj",
-        "hint": "Marca las horas y minutos."
-    },
-    {
-        "word": "Maleta",
-        "hint": "Se usa para llevar ropa de viaje."
-    },
-    {
-        "word": "Helado",
-        "hint": "Postre frío y delicioso."
-    },
-    {
-        "word": "Astronomía",
-        "hint": "Ciencia que estudia el espacio."
-    },
-    {
-        "word": "Biblioteca",
-        "hint": "Lugar lleno de libros."
-    },
-    {
-        "word": "Experimento",
-        "hint": "Prueba para investigar algo."
-    },
-    {
-        "word": "Galaxia",
-        "hint": "Conjunto de estrellas y planetas."
-    },
-    {
-        "word": "Microscopio",
-        "hint": "Instrumento para ver cosas pequeñas."
-    },
-    {
-        "word": "Ecología",
-        "hint": "Estudia la relación entre seres vivos y su entorno."
-    },
-    {
-        "word": "Química",
-        "hint": "Ciencia de elementos y compuestos."
-    },
-    {
-        "word": "Matemáticas",
-        "hint": "Trata de números y operaciones."
-    }    
-]
+import words from "./words.js";
 
-const wordText = document.querySelector(".word");
-const hintText = document.querySelector(".hint span");
-const timeText = document.querySelector(".timer span");
-const newWordButton = document.querySelector(".new_word");
-const livesText = document.querySelector(".lives span");
-const checkButton = document.querySelector(".check_word");
-const playerInputWord = document.querySelector("input");
+// Definir las constantes
+const wordText = document.querySelector('.word');
+const hintText = document.querySelector('.hint span');
+const playerText = document.querySelector('.player span');
+const timeText = document.querySelector('.timer span');
+const newWordButton = document.querySelector('.new_word');
+const livesText = document.querySelector('.lives span');
+const checkButton = document.querySelector('.check_word');
+const playerInputWord = document.querySelector('input');
+const scoreText = document.querySelector('.score span');
+const winningScore = 25;
+const players = [];
 
-function startTimer(maxtime){
-    clearInterval(timer);
-    timer = setInterval(()=>{
-        if(maxtime > 0) {
-            maxtime--;
-            return timeText.innerText = maxtime;
-        }
-        clearInterval(timer);
-        alert(`El tiempo se acabo, ${correctWord.toUpperCase()} era la palabra correcta!`);
-        lives--;
-        
-        livesText.innerText= lives
-        startGame();
-    }, 1000)
-}
+// Definir variables del juego
+let correctWord, timer, lives, numberOfPlayers, currentPlayer;
 
-function startGame(){
-    if (lives === 0){
-        alert("no quedan mas vidas")
-        return;
-}
-    startTimer(30);
-    let randomWord = words[Math.floor(Math.random() * words.length)];
-    let splitedWords = randomWord.word.split("");
-    for (let i = splitedWords.length - 1; i > 0; i--){
-        let j = Math.floor(Math.random() * (i + 1));
-        let temporary = splitedWords[i];
-        splitedWords[i] = splitedWords[j];
-        splitedWords[j] = temporary;
+// Iniciar el temporizador
+function startTimer(maxtime) {
+  clearInterval(timer);
+  timer = setInterval(() => {
+    if (maxtime > 0) {
+      maxtime--;
+      timeText.innerText = maxtime;
+    } else {
+      clearInterval(timer);
+      endRound(false);
     }
-    wordText.innerText = splitedWords.join("");
-    hintText.innerText = randomWord.hint;
-    correctWord = randomWord.word.toLowerCase();
-    playerInputWord.value = "";
-    playerInputWord.setAttribute("maxlength", correctWord.length);
+  }, 1000);
 }
 
+// Iniciar un nuevo juego o acabarlo si no quedan vidas
+function startGame() {
+  playerText.innerText = players[currentPlayer].name;
+  scoreText.innerText = players[currentPlayer].score;
+
+  if (lives === 0) {
+    alert('Fin del juego. ¡No te quedan más vidas!');
+    return;
+  }
+
+  startTimer(30);
+  const randomWord = getRandomWord();
+  displayWord(randomWord);
+}
+
+// Obtener una palabra aleatoria
+function getRandomWord() {
+  return words[Math.floor(Math.random() * words.length)];
+}
+
+// Mezclar las letras de la palabra
+function shuffleWord(word) {
+  const splitedWords = word.split('');
+  for (let i = splitedWords.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [splitedWords[i], splitedWords[j]] = [splitedWords[j], splitedWords[i]];
+  }
+  return splitedWords.join('');
+}
+
+// Mostrar la palabra y su pista
+function displayWord(wordInfo) {
+  const shuffledWord = shuffleWord(wordInfo.word);
+  wordText.innerText = shuffledWord;
+  hintText.innerText = wordInfo.hint;
+  correctWord = wordInfo.word.toLowerCase();
+  playerInputWord.value = '';
+  playerInputWord.setAttribute('maxlength', correctWord.length);
+}
+
+// Finalizar una ronda del juego
+function endRound(isCorrect) {
+  clearInterval(timer);
+  if (!isCorrect) {
+    lives--;
+    livesText.innerText = lives;
+    alert(`El tiempo se agotó. La palabra correcta era "${correctWord.toUpperCase()}".`);
+  }
+  currentPlayer++;
+  if (currentPlayer >= numberOfPlayers) currentPlayer = 0;
+  startGame();
+}
+
+// Comprobar si la palabra ingresada es la correcta
+function checkWord() {
+  const userInputWord = playerInputWord.value.toLowerCase();
+  if (!userInputWord) {
+    alert('Por favor, ingrese una palabra.');
+    return;
+  }
+  if (userInputWord !== correctWord) {
+    alert(`Lo siento, "${userInputWord}" no es la palabra correcta.`);
+  } else {
+    const timeLeft = parseInt(timeText.innerText);
+    players[currentPlayer].score += timeLeft * 1;
+    scoreText.innerText = players[currentPlayer].score;
+    if (timeLeft >= winningScore) {
+      endGame();
+      return;
+    }
+    alert(`¡Correcto! "${userInputWord.toUpperCase()}" es la palabra correcta.`);
+  }
+  endRound(true);
+}
+
+// Finalizar el juego si se averigua en 5segs un o menos
+function endGame() {
+  document.getElementById('container').style.display = 'none';
+  const gameOverText = document.getElementById('gameOver');
+  gameOverText.innerText = `GAME OVER\n¡Enhorabuena, ${players[currentPlayer].name.toUpperCase()} ha ganado!`;
+  gameOverText.style.display = 'block';
+}
+
+// Inicialización del juego
 lives = 3;
 livesText.innerText = lives;
+numberOfPlayers = parseInt(prompt('Introduzca el número de jugadores'));
+for (let i = 0; i < numberOfPlayers; i++) {
+  players[i] = { name: `Jugador ${i + 1}`, score: 0 };
+}
+currentPlayer = 0;
 startGame();
 
-function checkWord(){
-    let userInputWord = playerInputWord.value.toLocaleLowerCase();
-    if (!userInputWord) return alert("Por favor, indroduzca una palabra!");
-    if (userInputWord !== correctWord)
-        return alert(`Vaya, ${userInputWord} no es la palabra correcta!`);
-    alert(`Enhorabuena, ${userInputWord.toUpperCase()} es la palabra correcta!`);
-    startGame();
-}
-
-newWordButton.addEventListener("click", startGame);
-checkButton.addEventListener("click", checkWord);
+// eventos de los botones
+newWordButton.addEventListener('click', startGame);
+checkButton.addEventListener('click', checkWord);
---
+++ js/words.js
@@ -0,0 +1,164 @@
+const words = [
+    {
+      word: 'Casa',
+      hint: 'Lugar donde vives.'
+    },
+    {
+      word: 'Perro',
+      hint: 'Un amigo peludo.'
+    },
+    {
+      word: 'Flor',
+      hint: 'Puede ser rosa o margarita.'
+    },
+    {
+      word: 'Sol',
+      hint: 'Fuente de luz y calor en el cielo.'
+    },
+    {
+      word: 'Lago',
+      hint: 'Cuerpo de agua más pequeño que un océano.'
+    },
+    {
+      word: 'Bicicleta',
+      hint: 'Medio de transporte con dos ruedas.'
+    },
+    {
+      word: 'Computadora',
+      hint: 'Dispositivo para navegar en internet.'
+    },
+    {
+      word: 'Jardín',
+      hint: 'Espacio con flores y plantas.'
+    },
+    {
+      word: 'Guitarra',
+      hint: 'Instrumento musical de cuerdas.'
+    },
+    {
+      word: 'Reloj',
+      hint: 'Marca las horas y minutos.'
+    },
+    {
+      word: 'Maleta',
+      hint: 'Se usa para llevar ropa de viaje.'
+    },
+    {
+      word: 'Helado',
+      hint: 'Postre frío y delicioso.'
+    },
+    {
+      word: 'Astronomía',
+      hint: 'Ciencia que estudia el espacio.'
+    },
+    {
+      word: 'Biblioteca',
+      hint: 'Lugar lleno de libros.'
+    },
+    {
+      word: 'Experimento',
+      hint: 'Prueba para investigar algo.'
+    },
+    {
+      word: 'Galaxia',
+      hint: 'Conjunto de estrellas y planetas.'
+    },
+    {
+      word: 'Microscopio',
+      hint: 'Instrumento para ver cosas pequeñas.'
+    },
+    {
+      word: 'Ecología',
+      hint: 'Estudia la relación entre seres vivos y su entorno.'
+    },
+    {
+      word: 'Química',
+      hint: 'Ciencia de elementos y compuestos.'
+    },
+    {
+      word: 'Matemáticas',
+      hint: 'Trata de números y operaciones.'
+    },
+    {
+      word: 'Telescopio',
+      hint: 'Dispositivo para observar objetos distantes.'
+    },
+    {
+      word: 'Invierno',
+      hint: 'La estación más fría del año.'
+    },
+    {
+      word: 'Dinosaurio',
+      hint: 'Criaturas prehistóricas gigantes.'
+    },
+    {
+      word: 'Familia',
+      hint: 'Grupo de personas relacionadas entre sí.'
+    },
+    {
+      word: 'Desayuno',
+      hint: 'Comida que se consume por la mañana.'
+    },
+    {
+      word: 'Montaña',
+      hint: 'Gran elevación de terreno.'
+    },
+    {
+      word: 'Amistad',
+      hint: 'Relación afectuosa entre amigos.'
+    },
+    {
+      word: 'Aventura',
+      hint: 'Viaje emocionante y arriesgado.'
+    },
+    {
+      word: 'Felicidad',
+      hint: 'Estado de alegría y satisfacción.'
+    },
+    {
+      word: 'Misterio',
+      hint: 'Algo que es desconocido o enigmático.'
+    },
+    {
+      word: 'Canción',
+      hint: 'Composición musical con letras.'
+    },
+    {
+      word: 'Otoño',
+      hint: 'Estación caracterizada por hojas caídas.'
+    },
+    {
+      word: 'Medicina',
+      hint: 'Ciencia que trata enfermedades.'
+    },
+    {
+      word: 'Pintura',
+      hint: 'Arte de crear imágenes en lienzo.'
+    },
+    {
+      word: 'Universidad',
+      hint: 'Institución de educación superior.'
+    },
+    {
+      word: 'Chocolate',
+      hint: 'Delicioso dulce hecho de cacao.'
+    },
+    {
+      word: 'Aventurero',
+      hint: 'Persona que busca emociones nuevas.'
+    },
+    {
+      word: 'Secretario',
+      hint: 'Persona que administra información.'
+    },
+    {
+      word: 'Viaje',
+      hint: 'Excursión a un lugar distante.'
+    },
+    {
+      word: 'Naturaleza',
+      hint: 'Mundo exterior sin intervención humana.'
+    }
+  ];
+
+  export default words;
